07f7b00a592aac741eb61859e2aa07dfac63b132
Christine Le <christine.le244@gmail.com>
2019-10-24 20:12:18 -0400

    Added some projects and an about page!

---
+++ js/firebase-sdk.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBUTUCpZn8vEq1k38CRAZ-qtpOlPOaJdfU",
+    authDomain: "my-personal-portfolio-b92bd.firebaseapp.com",
+    databaseURL: "https://my-personal-portfolio-b92bd.firebaseio.com",
+    projectId: "my-personal-portfolio-b92bd",
+    storageBucket: "my-personal-portfolio-b92bd.appspot.com",
+    messagingSenderId: "816626537934",
+    appId: "1:816626537934:web:5f08572aee6be1953b079f"
+  };
+
+  firebase.initializeApp(firebaseConfig);
